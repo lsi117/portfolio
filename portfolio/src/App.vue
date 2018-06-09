d4a2972bf7c520038a3171a9a1099d884eda17bf
lauren inacio <lauren_f23@yahoo.com>
2018-06-09 15:44:37 -0400

    added subdirectory/vue/nodemon and main pg

---
+++ portfolio/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main-wrapper">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Portfolio'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
